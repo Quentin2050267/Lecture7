38fe18e90de368c5edfcf67f7296bda7c8e59c47
PK <dcspkv@nus.edu.sg>
2024-03-05 20:35:11 +0800

    connection to DB

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+
+let db;
+async function connectToDb() {
+	const url = 'mongodb://localhost/Marvel';
+	const client = new MongoClient(url, { useNewUrlParser: true });
+	await client.connect();
+	console.log('Connected to MongoDB at', url);
+	db = client.db();
+	console.log(await db.collection('characters').find({}).toArray());
+}
 
 const app = express();
 
 app.use(express.static('public'));
 
+(async function () {
+await connectToDb();	
 app.listen(3000, function () {
-	  console.log('App started on port 3000 successfully');
+	  console.log('App start on port 3000 successfully');
 });
+
+})
