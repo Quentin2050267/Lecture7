c2ebfce5b8f8329f651ae68af0fd14471abdfdba
PK <dcspkv@nus.edu.sg>
2024-03-05 19:51:15 +0800

    Code for AnimeList testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,66 @@
-class IssueFilter extends React.Component {
-	  render() {
-		      const systemnameinfilter = this.props.name;
-		      return (
-			            <div>This is a placeholder for the issue filter in {systemnameinfilter}.</div>
-			          );
-		    }
-}
+async function fetchGraphQLData(url, query, variables={}) {
+	const response = await fetch(url, {
+	  method: 'POST',
+	  headers: {
+		'Content-Type': 'application/json',
+		// Add any additional headers here (e.g., Authorization for protected endpoints)
+	  },
+	  body: JSON.stringify({
+		query,
+		// variables: {}, // Uncomment and use if your query requires variables
+	  }),
+	});
+  
+	if (!response.ok) {
+	  throw new Error(`Network response was not ok: ${response.statusText}`);
+	}
+  
+	const resultjson = await response.json();
+	return resultjson.data
+  }
 
-class IssueTable extends React.Component {
-	  render() {
-		      const systemnameintable = this.props.name;
-		      return (
-			            <div>This is a placeholder for a table of issues in {systemnameintable}.</div>
-			          );
-		    }
-}
+class GraphQLAPILecture7 extends React.Component {
 
-class IssueAdd extends React.Component {
-	  
-	  render() {
-		      const systemnameinadd = this.props.name;
-		      return (
-			            <div>This is a placeholder for a form to add an issue in {systemnameinadd}.</div>
-			          );
-		    }
-}
-
-class IssueList extends React.Component {
+	  constructor()
+	  {
+		super();
+		this.state = {anime: []}
+	  }
+	  componentDidMount()
+	  {
+		this.loadData()
+	  }
+	  async loadData()
+	  {
+		const query = `{
+			Page {
+			  media {
+				siteUrl
+				title {
+				  english
+				  native
+				}
+				description
+			  }
+			}
+		  }`;
+		const data = await fetchGraphQLData ("https://graphql.anilist.co", query)
+		console.log(data.Page.media)
+		var listofanime = []
+		data.Page.media.forEach ( item => {listofanime.push(item.title.english)})
+		this.setState({anime: listofanime})
+	  }
 	  render() {
 		      const systemname = "IT5007 Bug Tracker";
 		      return (
-			            <React.Fragment>
-			              <h1>{systemname}</h1>
-			              <IssueFilter name={systemname}/>
-			              <hr />
-			              <IssueTable name={systemname}/>
-			              <hr />
-			              <IssueAdd name={systemname}/>
-			              <hr/>
-			            </React.Fragment>
+			            <div>
+							<h1>GraphQL API Test</h1>
+							{this.state.anime.map((item, pos) =><p key={pos}>{item}</p>)}
+						</div>
 			          );
 		    }
 }
 
-const element = <IssueList />;
+const element = <GraphQLAPILecture7 />;
 
 ReactDOM.render(element, document.getElementById('contents'));
