44500b71b96e254b8d80eff6ef601aa4f2bd7906
PK <dcspkv@nus.edu.sg>
2024-03-05 21:19:55 +0800

    GQL implemented

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const { ApolloServer } = require('apollo-server-express');
+const fs = require('fs');
 
 let db;
 async function connectToDb() {
@@ -15,10 +17,57 @@ const app = express();
 
 app.use(express.static('public'));
 
+const resolvers = {
+	Query: {
+		getAllCharactersinfo,
+		getCharacterinfo,
+	},
+	Mutation: {
+		addCharacter
+	}
+  };
+  async function addCharacter(_, args)
+  {
+	  console.log(args)
+	  const newcharacter = {'name': args.name, 'gender': args.gender}
+	  //console.log(newcharacter)
+	  result = await db.collection('characters').insertOne(newcharacter);
+	  return true
+  }
+  async function getCharacterinfo(_, args)
+  {
+	  cname = args.name
+	  console.log(cname)
+	  result = await db.collection('characters').find({name: cname}, {name:1, gender:1}).toArray();
+	  console.log(result)
+	  return result[0]
+  }  
+
+async function getAllCharactersinfo()
+{
+	result = await db.collection('characters').find({}, {name:1, gender:1}).toArray();
+	console.log(result)
+	return result
+
+}
+
+
+//Create GQL
+const server = new ApolloServer({
+	typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
+	resolvers,
+	formatError: error => {
+	  console.log(error);
+	  return error;
+	},
+  });
+  server.applyMiddleware({ app, path: '/graphql' });
+
+
 (async function () {
 await connectToDb();	
 app.listen(3000, function () {
 	  console.log('App start on port 3000 successfully');
 });
 
-})
+})();
